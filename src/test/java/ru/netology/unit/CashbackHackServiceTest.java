29147e1d71919e7e75c28054af07d16a43fe7cc4
Stanislav Filimonov <36230254+FilStas@users.noreply.github.com>
2020-03-06 10:23:47 +0400

    init commit

---
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void cashbackFrom900() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void cashbackFrom1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void cashbackFrom1001() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void cashbackFrom999() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+}
