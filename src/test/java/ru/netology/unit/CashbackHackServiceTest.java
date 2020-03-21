4abf87bb7314ba18db0601453d099d68607cb431
FilStas <filstas23@gmail.com>
2020-03-21 19:47:33 +0400

    исправление в тесте (1000 = 0)

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -23,7 +23,7 @@ class CashbackHackServiceTest {
         int amount = 1000;
 
         int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
+        int expected = 0;
 
         assertEquals(expected, actual);
     }
